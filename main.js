ed4a2f2762f2727774e4fa52715dfd85e73f92a0
Philippe16 <philippe.suenson@hotmail.com>
2022-03-18 09:02:03 +0100

    frontend used pair programmering

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-import "./styles/style.css"
-import "bootstrap/dist/css/bootstrap.css"
-import "./src/jokeFacade"
-import "./src/hobbyConnect" 
-// import jokeFacade from "./src/jokeFacade"
+// JavaScript Document
 
-
-
-/* 
-  Add your JavaScript for all exercises Below or in separate js-files, which you must the import above
-*/
-
-/* JS For Exercise-1 below */
-
-
-/* JS For Exercise-2 below */
-
-
-/* JS For Exercise-3 below */
-
-
-/* 
-Do NOT focus on the code below, UNLESS you want to use this code for something different than
-the Period2-week2-day3 Exercises
-*/
+import "./src/hobbyConnect"
--- src/hobbyConnect.js
+++ src/hobbyConnect.js
@@ -1,39 +1,101 @@
-const zipContainer_element = document.querySelector("#zipContainer");
-const hobbyContainer_element = document.querySelector("#hobbyContainer");
+// JavaScript Document
+
+// --------------------------------- DOCUMENT ELEMENTS ---------------------------------
+
+const result_section_element = document.querySelector("#result_section");
+
+const getZip_btn_element = document.querySelector("#getZipBtn");
+const getHobbies_btn_element = document.querySelector("#getHobbiesBtn");
 
 
+// ------------------------------------- FUNCTIONS -------------------------------------
 
-const getAllHobby = async () => {
-    const result =  await fetch("https://medina-projects.dk/HobbyConnect/api/HobbyConnect/hobby");
-    const data = await result.json();
-    let hobbies = "";
-    data.forEach(element =>   {
-        hobbies += element.hobby;
+const getAllZip = () => {
+  fetch("https://medina-projects.dk/HobbyConnect/api/HobbyConnect/zip")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
 
+    // Reset result section content
+    result_section_element.innerHTML = "";
+
+    const zipList_container_element = document.createElement("div");
+    zipList_container_element.setAttribute("id", "zipList_container");
+    zipList_container_element.classList.add("flexRow");
+
+    data.forEach(zip => {
+      let zip_container_element = document.createElement("div");
+      zip_container_element.classList.add("zip_container");
+      zip_container_element.innerHTML = zip;
+
+      zipList_container_element.appendChild(zip_container_element);
     });
-    //hobbyContainer_element.innerHTML = hobbies;
-    
+
+    result_section_element.appendChild(zipList_container_element);
+  }); // fetch END
+}
+
+const getAllHobbies = () => {
+  fetch("https://medina-projects.dk/HobbyConnect/api/HobbyConnect/hobby")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+
+    // Reset result section content
+    result_section_element.innerHTML = "";
+
+    const hobbyList_1stTr_element = document.createElement("tr");
+
+    const thTxt = ["Id", "Hobby", "Beskrivelse"];
+
+    for(let i = 0; i < thTxt.length; i++){
+      let hobbyList_th_element = document.createElement("th");
+      hobbyList_th_element.innerHTML = thTxt[i];
+      hobbyList_1stTr_element.appendChild(hobbyList_th_element);
+    }
+
+    const hobbyList_table_element = document.createElement("table");
+    hobbyList_table_element.setAttribute("id", "hobbyList_table");
+    hobbyList_table_element.appendChild(hobbyList_1stTr_element);
+
+    data.forEach(hobby => {
+      let hobbyRow_tr_element = document.createElement("tr");
+
+      let hobbyId_td_element = document.createElement("td");
+      hobbyId_td_element.innerHTML = hobby.id_HobbyDTO;
+      hobbyRow_tr_element.appendChild(hobbyId_td_element);
+
+      let hobbyName_td_element = document.createElement("td");
+      hobbyName_td_element.innerHTML = hobby.hobby;
+      hobbyRow_tr_element.appendChild(hobbyName_td_element);
+
+      let hobbyLink_element = document.createElement("a");
+      hobbyLink_element.innerHTML = hobby.desc;
+      hobbyLink_element.setAttribute("href",  hobby.desc);
+      hobbyLink_element.setAttribute("target", "_blank");
+
+      let hobbyDesc_td_element = document.createElement("td");
+      hobbyDesc_td_element.appendChild(hobbyLink_element);
+      hobbyRow_tr_element.appendChild(hobbyDesc_td_element);
+
+      hobbyList_table_element.appendChild(hobbyRow_tr_element);
+    });
+
+    result_section_element.appendChild(hobbyList_table_element);
+  }); // fetch END
 }
 
 
+// --------------------------------- FUNCTIONS INVOKES ---------------------------------
 
-const getAllZip = async () => {
-    const result =  await fetch("https://medina-projects.dk/HobbyConnect/api/HobbyConnect/zip");
-    const data = await result.json();
-//    zipContainer_element.innerHTML = data;
-}
+getZip_btn_element.addEventListener("click", (event) =>{
+  getAllZip();
+});
 
-const getAmountByHobby = async () => {
-    const result =  await fetch("https://medina-projects.dk/HobbyConnect/api/HobbyConnect/hobby/1/person/amount");
-    const data = await result.json();
-
-    console.log("Amount" + data);
-
-}
+getHobbies_btn_element.addEventListener("click", (event) =>{
+  getAllHobbies();
+});
 
 
-
-
-getAllHobby();
-getAllZip();
-getAmountByHobby();
